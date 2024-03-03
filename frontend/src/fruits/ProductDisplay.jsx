7d6b64909416260688af82f65dd579ec2d9fc343
Abdulshakur <ashakur.js24@gmail.com>
2024-03-03 18:06:07 +0300

    fixed bugs

--- frontend/src/fruits/ProductDisplay.jsx
+++ frontend/src/fruits/ProductDisplay.jsx
@@ -1,57 +1,73 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const ProductDisplay = ({item}) => {
-    const {name, id, price, ratingsCount, quantity, desc} =item;
-    const [prequantity, setQuantity] = useState(quantity);
-    const [coupon, setCoupon] = useState('');
-    const handleDecrease = () => {
-        if (prequantity > 1){
-            setQuantity(prequantity - 1)
-        }
+const ProductDisplay = ({ item }) => {
+  const { name, id, price, ratingsCount, quantity, desc } = item;
+  const [prequantity, setQuantity] = useState(quantity);
+  const [coupon, setCoupon] = useState("");
+
+  const handleDecrease = () => {
+    if (prequantity > 1) {
+      setQuantity(prequantity - 1);
     }
-    const handleIncrease = () => {
-        setQuantity(prequantity + 1)
-    }
+  };
+  const handleIncrease = () => {
+    setQuantity(prequantity + 1);
+  };
 
   return (
     <div>
-        <div>
-            <h4>{name}</h4>
-            <p className='rating'>
-                <i className='icofont-star'></i>
-                <i className='icofont-star'></i>
-                <i className='icofont-star'></i>
-                <i className='icofont-star'></i>
-                <i className='icofont-star'></i>
-                <span>{ratingsCount} reviews</span>
-            </p>
-            <h4>{price}</h4>
-            <p>{desc}</p>
-        </div>
-        <div>
-            <form>
-                {/* Cart */}
-                <div className='cart-plus-minus'>
-                    <div className='dec qtybutton' onClick={handleDecrease}>-</div>
-                    <input className='cart-plus-minus-box' type='text' name='qtybutton' id='qtybutton' value={prequantity} onChange={(e) => setQuantity(parseInt(e.target.value, 10))} />
-                    <div className='inc qtybutton' onClick={handleIncrease}>+</div>
-                </div>
-                {/* Coupon */}
-                <div className='discount-code mb-2'>
-                    <input type='text' placeholder='Enter Discount Code' onChange={(e) => setCoupon(e.target.value)} />
-                </div>
-                {/* Button */}
-                <button type='submit' className='lab-btn'>
-                    <span>Add To Cart</span>
-                </button>
-                <Link to='/cart-page' className='lab-btn bg-black'>
-                    <span>Check Out</span>
-                </Link>
-            </form>
-        </div>
+      <div>
+        <h4>{name}</h4>
+        <p className="rating">
+          <i className="icofont-star"></i>
+          <i className="icofont-star"></i>
+          <i className="icofont-star"></i>
+          <i className="icofont-star"></i>
+          <i className="icofont-star"></i>
+          <span>{ratingsCount} reviews</span>
+        </p>
+        <h4>{price}</h4>
+        <p>{desc}</p>
+      </div>
+      <div>
+        <form>
+          {/* Cart */}
+          <div className="cart-plus-minus">
+            <div className="dec qtybutton" onClick={handleDecrease}>
+              -
+            </div>
+            <input
+              className="cart-plus-minus-box"
+              type="text"
+              name="qtybutton"
+              id="qtybutton"
+              value={prequantity}
+              onChange={(e) => setQuantity(parseInt(e.target.value, 10))}
+            />
+            <div className="inc qtybutton" onClick={handleIncrease}>
+              +
+            </div>
+          </div>
+          {/* Coupon */}
+          <div className="discount-code mb-2">
+            <input
+              type="text"
+              placeholder="Enter Discount Code"
+              onChange={(e) => setCoupon(e.target.value)}
+            />
+          </div>
+          {/* Button */}
+          <button type="submit" className="lab-btn">
+            <span>Add To Cart</span>
+          </button>
+          <Link to="/cart-page" className="lab-btn bg-black">
+            <span>Check Out</span>
+          </Link>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductDisplay
+export default ProductDisplay;
--- frontend/src/fruits/SingleProduct.jsx
+++ frontend/src/fruits/SingleProduct.jsx
@@ -1,71 +1,93 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
-import Data from '../products.json'
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-// Import Swiper styles
-import 'swiper/css';
-import ProductDisplay from './ProductDisplay';
+import "swiper/css";
+import ProductDisplay from "./ProductDisplay";
 
 const SingleProduct = () => {
-  const [product,setProduct] = useState([]);
-  const {id} = useParams();
-  
+  const [product, setProduct] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [productsData, setProductsData] = useState([]); // Add a state for the whole dataset
+  const { id } = useParams();
+  const navigate = useNavigate();
+
   useEffect(() => {
-    fetch("/src/products.json").then(res => res.json()).then(data => setProduct(data))
-  }, [])
-  
-  const result =product.filter((p) => p.id === id);
-  
+    const handle = async () => {
+      const data = await import("/src/products.json").then(
+        (data) => data.default
+      );
+      setProductsData(data); // Store the fetched data
+
+      const product = data.find((p) => p.id === id) || {};
+      setProduct(product);
+      setCurrentIndex(data.findIndex((p) => p.id === id));
+    };
+
+    handle();
+  }, [id]);
+
+  const navigateTo = (offset) => {
+    const newIndex = Math.max(
+      0,
+      Math.min(currentIndex + offset, productsData.length - 1) // Use productsData for bounds
+    );
+
+    if (productsData[newIndex]) {
+      const nextProductId = productsData[newIndex].id;
+      setCurrentIndex(newIndex);
+      navigate(`/fruits/${nextProductId}`);
+    }
+  };
+
   return (
     <div>
-      <div className='shop-single padding-tb aside-bg'>
-        <div className='container'>
-          <div className='row justify-content-center'>
-            <div className='col-lg-8 col-12'>
+      <div className="shop-single padding-tb aside-bg">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-12">
               <article>
-                <div className='product-details'>
-                  <div className='row align-items-center'>
-                    <div className='col-md-6 col-12'>
-                      <div className='product-thumb'>
-                        <div className='swiper-container pro-single-top'>
-                        <Swiper className="mySwiper">
-                          {
-                            product.map((item, i) => (
-                              <SwiperSlide key={i}>
-                                <div className='single-thumb'>
-                                <img src={item.img} alt='' />
-                                </div>
-                              </SwiperSlide>
-                            ))
-                          }
-                        </Swiper>
-                        <div className='pro-single-next'>
-                          <i className='icofont-rounded-left'></i>
-                        </div>
-                        <div className='pro-single-prev'>
-                          <i className='icofont-rounded-right'></i>
-                        </div>
+                <div className="product-details">
+                  <div className="row align-items-center">
+                    <div className="col-md-6 col-12">
+                      <div className="product-thumb">
+                        <div className="swiper-container pro-single-top">
+                          <Swiper className="mySwiper">
+                            <SwiperSlide key={product.id}>
+                              <div className="single-thumb">
+                                <img src={`/${product.img}`} alt="" />
+                              </div>
+                            </SwiperSlide>
+                          </Swiper>
+                          <div
+                            className="pro-single-next"
+                            onClick={() => navigateTo(-1)}
+                          >
+                            <i className="icofont-rounded-left"></i>
+                          </div>
+                          <div
+                            className="pro-single-prev"
+                            onClick={() => navigateTo(1)}
+                          >
+                            <i className="icofont-rounded-right"></i>
+                          </div>
                         </div>
                       </div>
                     </div>
-                    <div className='col-md-6 col-12'>
-                      <div className='post-content'>
-                        {
-                          result.map(item => <ProductDisplay key={item.id} item={item}/>)
-                        }
+                    <div className="col-md-6 col-12">
+                      <div className="post-content">
+                        <ProductDisplay key={product.id} item={product} />
                       </div>
                     </div>
                   </div>
                 </div>
-                
               </article>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SingleProduct
+export default SingleProduct;
