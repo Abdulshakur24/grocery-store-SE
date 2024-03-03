1122e29e86be18f6e30233146e2281e000793756
Shanelle Carrie <shanelleomondi@gmail.com>
2024-03-03 21:52:25 +0300

    commit

---
+++ frontend/src/components/CartPage.jsx
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+
+const CartPage = () => {
+    const [cartItems, setcartItems] = useState([]);
+
+    useEffect(() =>{
+        const storedCartItems = JSON.parse(localStorage.getItem('cart')) || [];
+        setcartItems(storedCartItems);
+    }, [])
+
+    // calculate prices
+    const calculateTotalPrice = (item) => {
+        return item.price * item.quantity;
+    }
+
+    // handle quantity increase
+    const handleIncrease = (item) => {
+        item.quantity += 1;
+        setcartItems([...cartItems]);
+
+    // update local storage with new cart items
+     localStorage.setItem('cart', JSON.stringify(cartItems));
+    }
+     
+     // handle quantity increase
+    const handleDecrease = (item) => {
+        if(item.quantity > 1) {
+            item.quantity -= 1;
+            setcartItems([...cartItems]);
+
+             // update local storage with new cart items
+            localStorage.setItem('cart', JSON.stringify(cartItems));
+        }
+    };
+
+    // remove/delete items
+    const handleRemoveItem = (item) => {
+        const updatedCart = cartItems.filter((cartItem) => cartItem.id !== item.id);
+
+        //update cart
+        setcartItems(updatedCart);
+        updateLocalStorage(updatedCart);
+    }
+    const updateLocalStorage = (cart) => {
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }
+
+    // cart subtotal 
+    const cartSubTotal = cartItems.reduce((total, item) => {
+        return total + calculateTotalPrice(item);
+    }, 0)
+
+    // order total
+    const orderTotal = cartSubTotal;
+
+  return (
+    <div>
+        <div className='shop-cart padding-tb'>
+            <div className="container">
+                <div className="section-wrapper">
+                    {/* Cart top */}
+                    <div className='cart-top'>
+                        <table>
+                            <thread>
+                                <tr>
+                                    <th className='cat-product'>Item</th>
+                                    <th className='cat-price'>Price</th>
+                                    <th className='cat-quantity'>Quantity</th>
+                                    <th className='cat-toprice'>Total</th>
+                                    <th className='cat-edit'>Edit</th>
+                                </tr>
+                            </thread>
+                            {/* Table body*/}
+                            <tbody>
+                                {
+                                    cartItems.map((item, indx) => {
+                                        <tr key={indx}>
+                                            <td className='product-item cat-product'>
+                                                <div className='p-thumb'>
+                                                    <Link to='/fruits'><img src={item.img} /></Link>
+                                                </div>
+                                                <div className='p-content'>
+                                                <Link to='/fruits'>{item.name}</Link>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                    {/* Cart top ends */}
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CartPage
