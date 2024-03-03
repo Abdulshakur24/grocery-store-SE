3605fdcc54510df1f30722377b303ec276be9314
Shanelle Carrie <shanelleomondi@gmail.com>
2024-03-03 15:33:43 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
-import { Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home";
-import Layout from "./Layout";
+import { useState } from 'react'
+import './App.css'
+import { Outlet } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-      </Route>
-    </Routes>
-  );
+    <>
+      <NavBar/>
+      <div className='min-vh-100'>
+      <Outlet/>
+      </div>
+      <Footer/>
+    </>
+  )
 }
 
-export default App;
+export default App
