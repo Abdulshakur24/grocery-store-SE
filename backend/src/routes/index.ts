a89175e836e4437ea256e4bda02e12f4d8fae3d2
Abdulshakur <ashakur.js24@gmail.com>
2024-03-17 16:55:53 +0300

    added checkout route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,4 +1,7 @@
 import { Router } from "express";
 import userRoutes from "./user";
+import checkoutRoutes from "./checkout";
 
-export default Router().use("/user", userRoutes);
+export default Router()
+  .use("/user", userRoutes)
+  .use("/checkout", checkoutRoutes);
